7c8c4d32619cf2180038b5854a8ab55e21d298f1
nievon <nievonclub@gmail.com>
2025-05-18 16:58:18 +0300

    Update frameworks

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,4 @@
 <?php
 
-$router->get('/', ['ProductController', 'index']); // Главная: список товаров
-
-$router->get('/product/{id}', ['ProductController', 'show']); // Просмотр одного товара
-
-$router->post('/cart/add', ['ProductController', 'addToCart']); // Добавить товар в корзину
-
-$router->get('/cart', ['ProductController', 'cart']); // Просмотр корзины
-
-$router->post('/cart/remove/{id}', ['ProductController', 'removeFromCart']); // Удаление из корзины
+$router->get('/', ['HomeController', 'index']);
+$router->get('/posts', ['HomeController', 'posts']);
