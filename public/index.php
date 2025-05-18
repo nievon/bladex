3138dc24a610bf1fc2d66034ac2f2ea3e2544f96
nievon <nievonclub@gmail.com>
2025-05-18 16:10:30 +0300

    500 error

--- public/index.php
+++ public/index.php
@@ -8,29 +8,41 @@ use Core\Env;
 use Core\View;
 use Core\Database;
 use Core\Middleware;
+
+try {
 // Загружаем переменные окружения
-Env::load(__DIR__ . '/../');
+    Env::load(__DIR__ . '/../');
 
 // Twig
-View::init(__DIR__ . '/../app/Views');
+    View::init(__DIR__ . '/../app/Views');
 
 // Подключение к БД из .env
-Database::init([
-    'host' => $_ENV['DB_HOST'],
-    'database' => $_ENV['DB_NAME'],
-    'username' => $_ENV['DB_USER'],
-    'password' => $_ENV['DB_PASS'],
-]);
+    Database::init([
+        'host' => $_ENV['DB_HOST'],
+        'database' => $_ENV['DB_NAME'],
+        'username' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASS'],
+    ]);
 
 // Роутинг
-$router = new Router();
+    $router = new Router();
 
 // Подключаем маршруты
-require_once __DIR__ . '/../routes/web.php';
-require_once __DIR__ . '/../core/Helpers.php';
+    require_once __DIR__ . '/../routes/web.php';
+    require_once __DIR__ . '/../core/Helpers.php';
 
 // Обрабатываем запрос
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$method = $_SERVER['REQUEST_METHOD'];
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $method = $_SERVER['REQUEST_METHOD'];
 
-$router->dispatch($uri, $method);
+    $router->dispatch($uri, $method);
+} catch (Throwable $e) {
+    http_response_code(500);
+
+    View::render('errors/500', [
+        'message' => $e->getMessage(),
+        'file' => $e->getFile(),
+        'line' => $e->getLine(),
+        'trace' => $e->getTraceAsString(),
+    ]);
+}
